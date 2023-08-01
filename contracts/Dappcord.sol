5b586037da8fa2ab54d44b4f3b96610d322d2ec0
Sumanjit38 <sumanjit38@gmail.com>
2023-08-01 23:59:06 +0530

    update

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -1,4 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappcord {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Dappcord is ERC721 {
+    uint256 public totalSupply;
+    uint256 public totalChannels = 0;
+    address public owner;
+
+    // Define a Channel
+    struct Channel {
+        uint256 id;
+        string name;
+        uint256 cost;
+    }
+
+    mapping(uint256 => Channel) public channels;
+    //mapping(uint256 => mapping(address => bool)) public hasJoined;
+
+    modifier onlyOwner() 
+    {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol) 
+        ERC721(_name, _symbol)
+    {
+            owner = msg.sender;
+    }
+
+    function createChannel(string memory _name, uint256 _cost) 
+        public onlyOwner 
+    {
+            // Create a Channel
+            //require(msg.sender == owner);
+            totalChannels++;
+            channels[totalChannels] = Channel(totalChannels, _name, _cost);
+    }
+
+    function getChannel(uint256 _id) public view returns (Channel memory) 
+    {
+        // Get a Channel
+        return channels[_id];
+    }
+}
